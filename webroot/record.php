3f574dd578cb4d3e23692b2c59ccbf73d23ce548
Matt Stone <mstone121@gmail.com>
2018-09-05 22:42:06 -0500

    Add record implementation

--- webroot/record.php
+++ webroot/record.php
@@ -1,3 +1,26 @@
 <?php
 
-print_r($_GET)
+if ($_SERVER['REMOTE_HOST'] !== 'mosx') {
+    // you are a loser (i.e. not me)
+    exit;
+}
+
+$config = json_decode(file_get_contents('../guideConfig.json'));
+
+$callsign = $_GET['callsign'];
+$station = $config->stationMap->$callsign;
+
+$airTime = new DateTime($_GET['airTime']);
+$airTime->setTimezone(new DateTimeZone('America/Chicago'));
+$airTime = $airTime->format('h:i Y-m-d');
+
+$duration = $_GET['duration'];
+
+$title = preg_replace('/[^A-Za-z0-9\s]/', '', $_GET['title']);
+$title = preg_replace('/\s+/', '_', $title);
+
+$execString = "at $airTime <<< 'record $station {$duration}s $title' 2>&1";
+
+$output = shell_exec($execString);
+echo "Exec: $execString" . PHP_EOL;
+echo "Output: " . PHP_EOL . $output;
